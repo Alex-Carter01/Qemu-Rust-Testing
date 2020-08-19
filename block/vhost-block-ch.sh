271cf395c5bc8bbe2e9ad63d678993b460881e76
Alex <alexjcarter@gmail.com>
2020-08-19 10:36:27 -0400

    Update vhost-block-ch.sh

--- block/vhost-block-ch.sh
+++ block/vhost-block-ch.sh
@@ -1,4 +1,4 @@
-#todo: path
-
+#todo: path and qcow2 image
+	
 /home/acarter/cloud-hypervisor/build/cargo_target/x86_64-unknown-linux-gnu/release/vhost_user_block \
-	--block-backend path=/tmp/example.qcow2,socket=/tmp/vhostqemu,num_queues=1,queue_size=128,readonly=false,direct=false,poll_queue=true
+	--block-backend path=/home/acarter/qemu-rust-testing/block/focal.qcow2,socket=/tmp/vhostqemu,num_queues=1,queue_size=128,readonly=false,direct=false,poll_queue=true
