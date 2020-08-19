bc9b29f0f29813e6edae6a9d5d120123d5a1ddae
alex carter <alexjcarter@gmail.com>
2020-08-19 15:03:52 +0000

    add filesystem mount to startup init

--- fs/startup_internet.sh
+++ fs/startup_internet.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-# Run this inside the guest machine at startup to finish setting up its internet connection
+# Run this inside the guest machine at startup to finish setup
+# Provides the interface and routes for its internet connection
 ip a
 dhclient ens3
+# Needs to load a kernel module to be able to mount the virtual filesystem
+modprobe virtiofs
+mount -t virtiofs myfs /mnt
+
