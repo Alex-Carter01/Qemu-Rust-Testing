5cf21980118966c451e4ab8b6fdb957cdce76be3
Alex <alexjcarter@gmail.com>
2020-08-19 10:35:06 -0400

    Update and rename OLD-qe-example.sh to qemu-block.sh

--- block/OLD-qe-example.sh
+++
@@ -1,8 +0,0 @@
-/home/acarter/qemu-5.0.0/x86_64-softmmu/qemu-system-x86_64 \
- 	-enable-kvm \
-	-drive file=/tmp/focal-server-cloudimg-amd64-disk-kvm.img,media=disk,if=virtio \
-       	-nographic \
-	-drive file=/tmp/nocloud.img,if=virtio -m 4095 \
-	-chardev socket,id=char0,path=/tmp/vhostqemu \
-    	-device vhost-user-blk-pci,queue-size=1024,chardev=char0 \
-	-m 1024 -object memory-backend-file,id=mem,size=1G,mem-path=/dev/shm,share=on -numa node,memdev=mem \
---
+++ block/qemu-block.sh
@@ -0,0 +1,7 @@
+/home/acarter/qemu-5.0.0/x86_64-softmmu/qemu-system-x86_64 \
+ 	-enable-kvm \
+       	-nographic \
+	-chardev socket,id=char0,path=/tmp/vhostqemu \
+	-m 4G \
+	-object memory-backend-file,id=mem,size=4G,mem-path=/dev/shm,share=on -numa node,memdev=mem \
+	-device vhost-user-blk-pci,queue-size=128,chardev=char0
