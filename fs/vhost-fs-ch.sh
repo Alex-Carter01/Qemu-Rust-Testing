46fc08459abefbbf2808c31bf01939fe95807531
Alex <alexjcarter@gmail.com>
2020-08-19 10:29:25 -0400

    Update and rename vhost-ch-fs.sh to vhost-fs-ch.sh

--- fs/vhost-fs-ch.sh
+++ fs/vhost-fs-ch.sh
@@ -1,4 +1,4 @@
-rm /tmp/vhostqemu
+#todo: show path without username-specific location (env variable?)
 
 /home/acarter/cloud-hypervisor/build/cargo_target/x86_64-unknown-linux-gnu/release/vhost_user_fs \
 	--socket /tmp/vhostqemu \
