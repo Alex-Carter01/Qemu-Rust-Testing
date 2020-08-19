8edf93df115656a54872437c1368b5e8e0361eac
Alex <alexjcarter@gmail.com>
2020-08-19 10:37:06 -0400

    Update and rename ch-net.sh to vhost-net-ch.sh

--- net/vhost-net-ch.sh
+++ net/vhost-net-ch.sh
@@ -1,4 +1,2 @@
-rm /tmp/vhostqemu
-
 /home/acarter/cloud-hypervisor/build/cargo_target/x86_64-unknown-linux-gnu/release/vhost_user_net \
 	--net-backend ip=127.0.0.1,mask=255.0.0.0,socket=/tmp/vhostqemu,num_queues=1,queue_size=128 #,tap=
